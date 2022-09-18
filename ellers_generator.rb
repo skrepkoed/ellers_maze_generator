2ac8c9d0a34cdbc438610d9c0a580de2afbbdb51
skrepkoed <artempecenkin240@gmail.com>
2022-09-18 12:04:31 +0500

    Maze can be drawn by rmagic

--- ellers_generator.rb
+++ ellers_generator.rb
@@ -37,7 +37,7 @@ class Row
       unless floor
         next_row.sets[i]=self.sets[i]
       else
-        next_row.sets[i]=next_row.sets.max+1
+        next_row.sets[i]=self.sets.max+1
       end
     end
     next_row
@@ -57,8 +57,41 @@ class Maze
     y.times do |rows|
       @maze<<@maze.last.create_next_row.join_adjecent.make_passage
     end
+    @maze.last.walls.map!{|wall| wall=false }
   end
 end
+maze=Maze.new(6,6)
+x=Array.new(9){|i| i*80}
+y=Array.new(9){|i| i*60}
+imgl = Magick::ImageList.new
+imgl.new_image(480, 420)
+gc = Magick::Draw.new
+gc.stroke_width(3)
+gc.stroke('black')
+
+
+
+maze.maze.each_with_index do |rows_cells, row|
+  rows_cells.walls.each_with_index do |wall,index|
+    if wall
+      gc.line(x[index+1],y[row],x[index+1],y[row+1])
+    end
+  end
+  rows_cells.floors.each_with_index do |floor,index|
+    if floor
+      gc.line(x[index],y[row+1],x[index+1],y[row+1])
+    end
+  end
+end
+
+gc.draw(imgl)
+imgl.border!(1,1, "LightCyan2")
+
+imgl.write("line2.gif")
+
+
+
+
 =begin
 r=Row.new(5)
 r.join_adjecent
@@ -76,6 +109,6 @@ print r1.walls
 puts "\n"
 print r1.floors 
 =end
-p Maze.new(5,1)
+p maze
 
 
