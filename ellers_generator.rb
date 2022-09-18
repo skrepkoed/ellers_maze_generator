a2ef74405778eb49658ceab9d8a67e75846d2c48
skrepkoed <artempecenkin240@gmail.com>
2022-09-18 10:07:37 +0500

    Method for next row created

--- ellers_generator.rb
+++ ellers_generator.rb
@@ -9,11 +9,11 @@ class Row
   end
 
   def join_adjecent
-    sets.each do |i|
+    sets.each_with_index do |k,i|
       if [true,false].sample
         if sets[i+1]
           walls[i]=false
-          sets[i+1]=i
+          sets[i+1]=k
         end
       end 
     end
@@ -28,6 +28,18 @@ class Row
       end
     end
   end
+
+  def create_next_row
+    next_row=Row.new(self.sets.size)
+    floors.each_with_index do |floor,i|
+      unless floor
+        next_row.sets[i]=self.sets[i]
+      else
+        next_row.sets[i]=next_row.sets.max+1
+      end
+    end
+    next_row
+  end
   private 
   def floor_map
     @sets.each_with_index do |k,index|
@@ -39,6 +51,15 @@ end
 r=Row.new(5)
 r.join_adjecent
 r.make_passage
-print r.sets
-print r.walls
-puts r.floors
+r1=r.create_next_row
+print r.sets 
+puts "\n"
+print r.walls 
+puts "\n"
+print r.floors 
+puts "\n"
+print r1.sets
+puts "\n" 
+print r1.walls 
+puts "\n"
+print r1.floors 
